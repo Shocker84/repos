e61163d8f201258e91ec0ce00a5d4346b0e57c2b
James Filbey <james.r.filbey@gmail.com>
2021-06-22 20:56:02 -0500

    first commit

---
+++ School stuff 2/School stuff 2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace School_stuff_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int[] arr = {1, 2, 3, 4, 5};
+
+              for (int i = 0; i < arr.Length; i++)
+              {
+                  Console.Write(arr[i] + " ");
+              }
+
+              try
+              {
+                  Console.WriteLine(arr[7]);
+              }
+              catch (IndexOutOfRangeException e)
+              {
+                  Console.WriteLine("An IndexOut OfRangeException has occured: {0} {1} {2} {1}",
+                          e.Message, 77, 123);
+              }
+
+              catch (Exception e)
+              {
+                  Console.WriteLine("An Exception has occured: {0}", e.Message);
+              }
+              */
+
+            int[] arr2 = { 19, 2, 75, 52 };
+
+            try
+            {
+                for (int i = 0; i < arr2.Length - 1; i++)
+                {
+                    Console.WriteLine(arr2[i] / arr2[i + 1]);
+                }
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                Console.Write("finally block");
+                for(int i = 0; i < arr2.Length; i++)
+                {
+                    Console.Write(arr2[i] + " ");
+                }
+            }
+
+
+
+            Console.WriteLine("\n\nHave a nice day!");
+        }
+    }
+}
---
+++ assignment 2/assignment 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 0 and 255: ");
+
+            string n1 = Console.ReadLine();
+
+            Console.WriteLine("Enter another number between 0 and 255: ");
+
+            string n2 = Console.ReadLine();
+
+            try
+            {
+                int num1 = Int32.Parse(n1);
+                int num2 = Int32.Parse(n2);
+                int ans = Int32.Parse(n1) / Int32.Parse(n2);
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nFormatException: " + e.Message);
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("\nDivideByZeroException: " + e.Message);
+            }
+
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("\nArgumentOutOfRangeException: " + e.Message);
+            }
+
+            Console.WriteLine("\n" + n1 + " divided by " + n2 + " is " + Int32.Parse(n1) / Int32.Parse(n2));
+
+
+            Console.WriteLine("\nHave a great day!");
+        }
+    }
+}
